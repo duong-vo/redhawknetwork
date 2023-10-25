49977f5094769082437423150efd493aa3211724
LandonDeamMiami <112983537+LandonDeamMiami@users.noreply.github.com>
2023-10-25 14:38:09 -0400

    big refactoring

--- backend/src/main/java/com/redhawknetwork/controller/UserController.java
+++ backend/src/main/java/com/redhawknetwork/controller/UserController.java
@@ -3,13 +3,12 @@ package com.redhawknetwork.controller;
 import com.redhawknetwork.service.UserService;
 import com.redhawknetwork.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+@RestController
+@RequestMapping("/api/")
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class UserController {
 
 	@Autowired
